9e87b471b05a7c707febf9497f7963716b8d3d09
martin <poehlmann.martin@gmail.com>
2019-09-27 05:32:34 +0200

    initial

---
+++ giant_chrome.ps1
@@ -0,0 +1,5 @@
+﻿$chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+$params = '/high-dpi-support=1.75 /force-device-scale-factor=1.75'
+
+$params = $params.Split(" ")
+& "$chromePath" $Prms
