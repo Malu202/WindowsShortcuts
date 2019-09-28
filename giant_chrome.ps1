e7b98c67b28a086294936dc86b14eeb85fc457d2
martin <poehlmann.martin@gmail.com>
2019-09-28 06:04:21 +0200

    add shortcuts via powershell script

--- giant_chrome.ps1
+++ giant_chrome.ps1
@@ -1,5 +1,9 @@
-﻿$chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$params = '/high-dpi-support=1.75 /force-device-scale-factor=1.75'
+﻿#$chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+#$params = '/high-dpi-support=1.75 /force-device-scale-factor=1.75'
 
-$params = $params.Split(" ")
-& "$chromePath" $Prms
+#$params = $params.Split(" ")
+#& "$chromePath" $Prms
+
+$RegConnect = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]”CurrentUser”,”$env:COMPUTERNAME”)
+
+$RegCursors = $RegConnect.OpenSubKey(“Control Panel\Desktop”,$true)
